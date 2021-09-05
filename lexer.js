ff6329c9fcd1e2c7deb14aeb3ff361b86ed50947
José Antonio Lome Paulino <50527508+Joeypph@users.noreply.github.com>
2021-09-04 19:29:44 -0500

    Se hace validación con espacio en blanco, se ocupa REGEX para los números

--- lexer.js
+++ lexer.js
@@ -1,24 +1,62 @@
-const input = "7 77";
+const input = "77 7   8 2 1 232 ";
 
 function* lexer(str) {
-  for (let cursor = 0; cursor <= str.length; cursor++) {
-    let chr = str[cursor]
-    if(chr === undefined){
-      yield { 
-        type: 'EOF'
+  let cursor = 0;
+  let chr = str[cursor];
+
+  //especial  insise iterator
+  function next() {
+    cursor++;
+    chr = str[cursor];
+  }
+  function number() {
+    let buffer = "";
+    while (/^\d+$/.test(chr)) {
+      buffer += chr;
+      next();
+    }
+    if (buffer.length >= 1) {
+      return { type: "number", buffer };
+    }
+    return null;
+  }
+  function eof() {
+    chr = str[cursor];
+    if (chr === undefined) {
+      cursor++;
+      return {
+        type: "EOF",
+      };
+    }
+    return null;
+  }
+  function whiteSpace() {
+    let buffer = "";
+    while (chr === " " || chr === "\t") {
+      next();
+    }
+
+    return null;
+  }
+
+  //version corta de while(true) que no requiere optimización
+  for (;;) {
+    whiteSpace();
+    const token = number() || eof();
+    if (token) {
+      yield token;
+      if (token.type === "EOF") {
+        break;
       }
-    }else if (chr === "7"){
-      yield { 
-        type: 'number',
-        value: 7,
-      }
+    } else {
+      throw new SyntaxError(`Caracter no reconocido : "${chr}" at ${cursor + 1}`);
     }
   }
 }
 
-console.log('Start');
-for(const token of lexer(input)){
+console.log("Inicio");
+for (const token of lexer(input)) {
   console.log(token);
 }
-
-console.log('Finish');
+//console.log([...lexer(input)]);
+console.log("Final");
