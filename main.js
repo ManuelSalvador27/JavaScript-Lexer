4941fbce4d91b79ef33e28462a36b4a9645abc79
ManuelSalvador27 <A01422267@itesm.mx>
2021-09-11 21:05:22 -0500

    Lectura e impresión de cada token

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import { readFileSync } from "fs"
 import { lexer } from "./lexer.js"
-import { parser } from "./decompose.js"
 
+/*
+BEFORE: Input of code like a string
+const input= `1234`;
+*/ 
 const filename = "./test.js"
 const input = String(readFileSync(filename))
 
 console.log("Lexer trabajando");
-const tokens = parser (lexer(filename, input))
-console.log(tokens)
+const tokens = lexer(filename, input)
+for (const token of tokens) {
+  console.log(token);
+}
